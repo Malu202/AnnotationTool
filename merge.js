52d3e0dd51a22c3e206c9a3e5279662ee588b10d
martin <poehlmann.martin@gmail.com>
2020-07-12 03:57:12 +0200

    start with csv merger

---
+++ merge.js
@@ -0,0 +1,212 @@
+let splitCheckBox = document.getElementById("splitCheckBox");
+let splitRatioContainer = document.getElementById("splitRatioContainer");
+let splitRatio = document.getElementById("splitRatio");
+let fileInput = document.getElementById("fileInput");
+let progressBar = document.getElementById("progressBar")
+
+splitCheckBox.addEventListener("change", function () {
+    if (splitCheckBox.checked) splitRatioContainer.style.visibility = "visible";
+    else splitRatioContainer.style.visibility = "hidden";
+})
+splitCheckBox.dispatchEvent(new Event("change"))
+
+fileInput.addEventListener("change", function () {
+    merge(fileInput.files);
+});
+
+var input = './hantel/111hantel.csv';
+var trainingOutput = './hantel/111train.csv';
+var validationOutput = './hantel/111eval.csv';
+var classesOutput = './hantel/111classes.csv';
+
+var csvByImage = {};
+
+
+
+function merge(files) {
+    let outputString = "";
+    let fileIndex = 0;
+
+    loadCsvFiles(files, fileIndex, outputString, function (outputString) {
+        verify(outputString)
+    })
+
+}
+function loadCsvFiles(files, fileIndex, outputString, cb) {
+    let type = files[fileIndex].type.split('/')[0];
+
+    progressBar.value = 0;
+    progressBar.visibility = "visible";
+
+    var fr = new FileReader();
+    fr.onload = function () {
+        fileIndex++;
+        outputString += fr.result;
+        progressBar.value = Math.round((fileIndex / files.length) * 100);
+        if (fileIndex == files.length - 1) {
+            progressBar.visibility = "hidden";
+            cb(outputString)
+        }
+        else loadCsvFiles(files, fileIndex, outputString, cb)
+    }
+
+    if (type == "text") {
+        fr.readAsText(files[fileIndex]);
+    }
+}
+
+let outputLogDiv = document.getElementById("outputLog");
+function verify(csv) {
+    let outputLog = "";
+    var csv = csv.split(/\r?\n/);
+
+    for (var i = 0; i < csv.length; i++) {
+        if (csv[i] == "") continue;
+        var data = csv[i].split(',');
+        var imgPath = 0;
+        var x1 = 1;
+        var y1 = 2;
+        var x2 = 3;
+        var y2 = 4;
+        var klasse = 5;
+
+        data[x1] = parseInt(data[x1]);
+        data[y1] = parseInt(data[y1]);
+        data[x2] = parseInt(data[x2]);
+        data[y2] = parseInt(data[y2]);
+
+
+        if (data[x1] == data[x2] || data[y1] == data[y2]) {
+            csv.splice(i, 1);
+            outputLog += "Removed line " + (i + 1) + " (zero area)" + ": " + data + "\n";
+            continue;
+        }
+
+        var flipped = false;
+        var old = data.toString();
+        if (data[x1] > data[x2]) {
+            var oldX1 = data[x1];
+            data[x1] = data[x2];
+            data[x2] = oldX1;
+            // console.log(data[x1], data[x2])
+            flipped = true;
+        }
+        if (data[y1] > data[y2]) {
+            var oldy1 = data[y1];
+            data[y1] = data[y2];
+            data[y2] = oldy1;
+            flipped = true;
+        }
+        if (flipped) {
+            outputLog += "Flipped line " + (i + 1) + ": " + old + "\n"
+            outputLog += "       New line: " + data + "\n";
+        }
+        //csv[i] = data.toString();
+        if (!csvByImage[data[imgPath]]) csvByImage[data[imgPath]] = [];
+        csvByImage[data[imgPath]].push(data.toString());
+
+    }
+
+    var imageCount = Object.keys(csvByImage).length;
+
+    var split = 1;
+    if (splitCheckBox.checked) split = splitRatio.value;
+    var trainCount = Math.round(imageCount * split);
+    var evalCount = Math.round(imageCount * (1 - split));
+
+    outputLog += "=======================\n"
+    outputLog += "Found " + imageCount + " valid images, splitting into " + trainCount + " training and " + evalCount + " validating images" + '\n';
+
+    var trainCsv = [];
+    var evalCsv = [];
+
+    for (var i = 0; i < evalCount; i++) {
+        var selected = Math.round(Math.random() * (imageCount - 1));
+
+        var selectedImage = Object.keys(csvByImage)[selected];
+
+        evalCsv.push(csvByImage[selectedImage].join('\n'));
+        delete csvByImage[selectedImage];
+
+        imageCount--;
+    }
+    for (var j = 0; j < imageCount; j++) {
+        var selectedImage = Object.keys(csvByImage)[j];
+        trainCsv.push(csvByImage[selectedImage].join('\n'))
+    }
+
+    outputLogDiv.innerText = outputLog;
+
+    // saveFile(trainingOutput, trainCsv.join('\n'));
+    // saveFile(validationOutput, evalCsv.join('\n'));
+
+
+    console.log("Training data: " + trainCsv.length + " images, saved to " + trainingOutput);
+    console.log("Evaluation data: " + evalCsv.length + " images, saved to " + validationOutput);
+
+    if (split != 1) getClasses(trainCsv, evalCsv);
+    // console.log(evalCsv.toString())
+
+
+}
+
+function getClasses(train, eval) {
+
+    var trainCsv = [];
+    var evalCsv = [];
+    for (var l = 0; l < train.length; l++) {
+        var entries = train[l].split(/\r?\n/);
+        trainCsv = trainCsv.concat(entries);
+    }
+    for (var n = 0; n < eval.length; n++) {
+        var entries = eval[n].split(/\r?\n/);
+        evalCsv = evalCsv.concat(entries);
+    }
+
+    var trainClasses = {};
+    for (var i = 0; i < trainCsv.length; i++) {
+        if (!trainClasses[trainCsv[i].split(',')[5]]) trainClasses[trainCsv[i].split(',')[5]] = 0;
+        trainClasses[trainCsv[i].split(',')[5]]++;
+    }
+
+    var evalClasses = {};
+    for (var j = 0; j < evalCsv.length; j++) {
+        if (!evalClasses[evalCsv[j].split(',')[5]]) evalClasses[evalCsv[j].split(',')[5]] = 0;
+        evalClasses[evalCsv[j].split(',')[5]]++;
+    }
+
+    var allClasses = {};
+    for (var key1 in trainClasses) {
+        if (!allClasses[key1]) allClasses[key1] = 0;
+        allClasses[key1] += trainClasses[key1];
+    }
+    for (var key2 in evalClasses) {
+        if (!allClasses[key2]) allClasses[key2] = 0;
+        allClasses[key2] += evalClasses[key2];
+    }
+    console.log();
+    console.log("Split per classes: ")
+    var classes = Object.keys(allClasses);
+    var classesFile = "";
+
+    //sort alphabetically so its always the same:
+    classes.sort();
+
+    for (var k = 0; k < classes.length; k++) {
+        classesFile += classes[k] + ',' + k + '\n';
+
+        var classTrain = trainClasses[classes[k]]
+        var classEval = evalClasses[classes[k]];
+        var classSplit = classTrain / allClasses[classes[k]];
+        classSplit = Math.round(classSplit * 10000) / 100
+        console.log(classes[k] + ": " + classSplit + "% (" + classTrain + "|" + classEval + ")")
+    }
+    //saveFile(classesOutput, classesFile)
+}
+
+function saveFile(content, name) {
+    var a = document.createElement("a");
+    a.download = name;
+    a.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(content);
+    a.click();
+}
